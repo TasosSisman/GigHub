0727dc51d0b253a08fd503040dcba6bc41fa0699
TasosSisman <tasossisman@gmail.com>
2020-11-02 01:45:09 +0200

    Moved DateTime Parsing to GigFormViewModel

--- ViewModels/GigFormViewModel.cs
+++ ViewModels/GigFormViewModel.cs
@@ -8,10 +8,20 @@ namespace GigHub.ViewModels
 {
 	public class GigFormViewModel
 	{
+
 		public string Venue { get; set; }
 		public string Date { get; set; }
 		public string Time { get; set; }
 		public byte Genre { get; set; }
 		public IEnumerable<Genre> Genres { get; set; }
+		
+		public DateTime DateTime
+		{
+			get
+			{
+				return DateTime.Parse(string.Format("{0} {1}", Date, Time));
+			}
+		}
+
 	}
 }
