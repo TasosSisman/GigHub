98fc3b23a95fb4697b542fdc89040602e8bf99c9
TasosSisman <tasossisman@gmail.com>
2020-11-02 02:06:17 +0200

    Added server-side validation for Date and Time in Gig Create.

--- Controllers/GigsController.cs
+++ Controllers/GigsController.cs
@@ -33,11 +33,15 @@ namespace GigHub.Controllers
         [HttpPost]
         public ActionResult Create(GigFormViewModel viewModel)
 		{
+            if (!ModelState.IsValid)
+			{
+                return View("Create", viewModel);
+			}
 
 			var gig = new Gig
 			{
                 ArtistId = User.Identity.GetUserId(),
-                DateTime = viewModel.DateTime,
+                DateTime = viewModel.GetDateTime(),
                 GenreId = viewModel.Genre,
                 Venue = viewModel.Venue
             };
