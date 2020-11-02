6f50a75c0c903815ed79b666464927c6015ed398
TasosSisman <tasossisman@gmail.com>
2020-11-02 12:45:59 +0200

    Added api for changing Attendance status

--- Models/ApplicationDbContext.cs
+++ Models/ApplicationDbContext.cs
@@ -18,5 +18,16 @@ namespace GigHub.Models
 
         public DbSet<Gig> Gigs { get; set; }
         public DbSet<Genre> Genres { get; set; }
-    }
+		public DbSet<Attendance> Attendances { get; set; }
+
+		protected override void OnModelCreating(DbModelBuilder modelBuilder)
+		{
+            modelBuilder.Entity<Attendance>()
+                .HasRequired(async => async.Gig)
+                .WithMany()
+                .WillCascadeOnDelete(false);
+
+			base.OnModelCreating(modelBuilder);
+		}
+	}
 }
