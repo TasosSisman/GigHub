2919551e035b07fbf637e7e7f72d718ae7728161
TasosSisman <tasossisman@gmail.com>
2020-11-02 14:53:08 +0200

    Made buttons in Home/index hidden when unauthorized.

---
+++ ViewModels/HomeViewModel.cs
@@ -0,0 +1,13 @@
+﻿using GigHub.Models;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GigHub.ViewModels
+{
+	public class HomeViewModel
+	{
+		public IEnumerable<Gig> upcomingGigs { get; set; }
+		public bool ShowActions { get; set; }
+	}
+}
