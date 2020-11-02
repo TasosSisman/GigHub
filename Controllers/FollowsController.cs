08016e0272467769ea9453077f6072fa370d83f1
TasosSisman <tasossisman@gmail.com>
2020-11-02 13:57:27 +0200

    Added api for Following functionality.

---
+++ Controllers/FollowsController.cs
@@ -0,0 +1,46 @@
+﻿using GigHub.Dtos;
+using GigHub.Models;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace GigHub.Controllers
+{
+	public class FollowsController : ApiController
+	{
+		private readonly ApplicationDbContext _context;
+
+		public FollowsController()
+		{
+			_context = new ApplicationDbContext();
+		}
+
+		[Authorize]
+		[HttpPost]
+		public IHttpActionResult Follow(FollowDto dto)
+		{
+			var userId = User.Identity.GetUserId();
+
+			if(_context.Followings.Any(f => f.FollowerId == userId && f.FolloweeId == dto.FolloweeId))
+			{
+				return BadRequest("Already Following.");
+			}
+
+			var following = new Following
+			{
+				FollowerId = userId,
+				FolloweeId = dto.FolloweeId
+			};
+
+			_context.Followings.Add(following);
+			_context.SaveChanges();
+
+			return Ok();
+		}
+
+	}
+}
--- Models/ApplicationDbContext.cs
+++ Models/ApplicationDbContext.cs
@@ -19,6 +19,7 @@ namespace GigHub.Models
         public DbSet<Gig> Gigs { get; set; }
         public DbSet<Genre> Genres { get; set; }
 		public DbSet<Attendance> Attendances { get; set; }
+		public DbSet<Following> Followings { get; set; }
 
 		protected override void OnModelCreating(DbModelBuilder modelBuilder)
 		{
@@ -27,7 +28,17 @@ namespace GigHub.Models
                 .WithMany()
                 .WillCascadeOnDelete(false);
 
-			base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<ApplicationUser>()
+                .HasMany(u => u.Followers)
+                .WithRequired(f => f.Followee)
+                .WillCascadeOnDelete(false);
+
+            modelBuilder.Entity<ApplicationUser>()
+                .HasMany(u => u.Followees)
+                .WithRequired(f => f.Follower)
+                .WillCascadeOnDelete(false);
+
+            base.OnModelCreating(modelBuilder);
 		}
 	}
 }
