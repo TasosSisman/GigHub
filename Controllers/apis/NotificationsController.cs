ac168d6fb702085f10ab92e41cd604351cc02a72
TasosSisman <tasossisman@gmail.com>
2020-11-04 23:42:42 +0200

    Added DTOs for Notifications using AutoMapper.

---
+++ Controllers/apis/NotificationsController.cs
@@ -0,0 +1,40 @@
+﻿using AutoMapper;
+using GigHub.Dtos;
+using GigHub.Models;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace GigHub.Controllers.apis
+{
+	[Authorize]
+	public class NotificationsController : ApiController
+	{
+		private readonly ApplicationDbContext _context;
+
+		public NotificationsController()
+		{
+			_context = new ApplicationDbContext();
+		}
+
+		[HttpGet]
+		public IEnumerable<NotificationDto> GetNewNotifications()
+		{
+			var userId = User.Identity.GetUserId();
+			var notifications = _context.UserNotifications
+				.Where(u => u.UserId == userId && u.IsRead == false)
+				.Select(u => u.Notification)
+				.Include(u => u.Gig.Artist)
+				.ToList();
+
+			
+
+			return notifications.Select(Mapper.Map<Notification, NotificationDto>);
+		}
+	}
+}
